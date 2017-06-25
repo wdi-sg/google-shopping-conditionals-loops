52bfb0aa29ff83aba993faf79dae85b2a976547e
Wanzhen Ng <ngwanzhen@gmail.com>
2017-06-25 13:41:25 +0800

    google shopping functions q1-5

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -1,5 +1,56 @@
-var data = require('../products.json');
+var data = require('../products.json')
 
 // Write your solutions below
+var itemArr = []
 
-console.log(data.items);
+function getItems (data) {
+  data.items.map(function (e) {
+    itemArr.push(e)
+  })
+  return itemArr
+  // console.log(itemArr)
+}
+
+function getItemsByBrand (items, brandToMatch) {
+  var itemsByBrand = []
+  items.map(function (e) {
+    var brand = e.product.brand
+    if (brand == brandToMatch) { itemsByBrand.push(e) }
+  })
+  return itemsByBrand
+  // console.log(itemsByBrand)
+}
+
+function getItemsByAuthor (items, authorToMatch) {
+  var itemsByAuthor = []
+  items.map(function (e) {
+    var author = e.product.author.name
+    if (author == authorToMatch) { itemsByAuthor.push(e) }
+  })
+  return itemsByAuthor
+  // console.log(itemsByAuthor)
+}
+
+function getAvailableProduct (items) {
+  var availItems = []
+  items.map(function (e) {
+    var stock = e.product.inventories[0].availability
+    if (stock == 'inStock') { availItems.push(e) }
+  })
+  return availItems
+  // console.log(availItems)
+}
+
+// All items made by Sony.
+// var itemsBySony = getItemsByBrand(getItems(data), 'Sony')
+// console.log(itemsBySony)
+
+// All items made by Sony that are available.
+// console.log(getAvailableProduct(itemsBySony))
+
+// All available items by the author "Adorama Camera"
+// console.log(getItemsByAuthor(getItems(data), 'Adorama Camera'))
+
+// All items made by Nikon with the author eBay.
+var itemsByNikon = getItemsByBrand(getItems(data), 'Nikon')
+console.log(getItemsByAuthor(itemsByNikon, 'eBay').length)
