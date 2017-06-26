c7f7921ac690983421bb63278e7f91967f06f6f9
Daniel Lim <ddanielnp@hotmail.com>
2017-06-26 19:54:25 +0800

    google-shopping-functions done

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -4,6 +4,7 @@ var data = require('../products.json')
 // console.log(data.items)
 var items = data.items
 
+// GOOGLE SHOPPING CONDITIONAL LOOPS
 // 1.  Print the count of these results. Where else is this count information stored in the search results?
 var itemCount = 0
 
@@ -25,6 +26,7 @@ for (var j = 0; j < items.length; j++) {
   }
 }
 
+// GOOGLE SHOPPING FUNCTIONS
 // 1.) getItems(objectData)
 //
 // input: json object
@@ -38,7 +40,7 @@ function getItems () {
   return data.items
 }
 // getItems()
-var items = getItems() //to make it a global variable
+var items = getItems() // to make it a global variable
 // console.log(items)
 
 // 2.) getItemsByBrand(items, brand)
@@ -85,3 +87,41 @@ function getItemsByAuthor (items, searchAuthor) {
   return arrAuthor
 }
 getItemsByAuthor(items, 'eBay')
+
+// 4.) getAvailableProducts(items)
+//
+// input: an array of items
+// returns: an array of items (that are available)
+// Create function called getAvailableProducts that takes an item array and returns an array containing all of the available products (an available product is one with at least one availability of "inStock" in the inventories array)
+
+function getAvailableProducts (items) {
+  var itemsInStock = []
+  for (var m = 0; m < items.length; m++) {
+    var isAvail = items[m].product.inventories[0].availability
+    if (isAvail === 'inStock') {
+      itemsInStock.push(items[i].product.title)
+    }
+  } return itemsInStock
+}
+
+// 5.) Use your functions
+//
+// Use the functions you created in 1 - 5 to ouput (console.log) the following lists of items.
+//
+// a. All items made by Sony.
+var sonyItems = getItemsByBrand(items, 'Sony')
+// console.log(sonyItems)
+// b. All items made by Sony that are available.
+var sonyAvailable = getAvailableProducts(sonyItems)
+// console.log(sonyAvailable)
+// c. All available items by the author "Adorama Camera"
+var adoramaCamera = getItemsByAuthor(items, 'Adorama Camera')
+var adoramaAvailable = getAvailableProducts(adoramaCamera)
+// console.log(adoramaAvailable)
+// d. All items made by Nikon with the author eBay.
+var ebay = getItemsByAuthor(items, 'eBay')
+var eBayNikon = getItemsByBrand(ebay, 'Nikon')
+// console.log(eBayNikon)
+// Bonus
+//
+// Create another search function and/or think of other interesting ways to combine the functions to perform useful searches.
