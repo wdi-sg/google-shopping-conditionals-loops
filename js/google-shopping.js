239372d91c86c1bfa8cfd61fdaa48a96a6218b2c
Dominic Phua <dominikphua@gmail.com>
2017-06-26 16:55:49 +0800

    Finished question 1 & 2

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -1,5 +1,28 @@
 var data = require('../products.json');
+//
+// // ----------------------------------------------------------------
+//
+// var counter = 0
+//
+// for (var i = 0; i < data.items.length; i++) {
+//   if (data.items[i].kind == "shopping#product") {
+//     counter = counter + 1
+//   }
+// }
+//
+// console.log('The total number of shopping products is ' + counter)
+//
+// console.log(data.items[1].kind)
+//
+// ----------------------------------------------------------------
+//
+// console.log('List of backorder items:')
+//
+// for (var i = 0; i < data.items.length; i++) {
+//   if (data.items[i].product.inventories[0].availability === "backorder") {
+//     console.log(data.items[i].product.title)
+//   }
+// }
 
-// Write your solutions below
-
-console.log(data.items);
+// console.log(data.items[0].product.inventories[0].availability)
+// console.log(data.items[0].product.title)
