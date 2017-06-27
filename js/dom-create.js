8da6c5de7f3b2c4d1c3389892f083a6c2d20285b
Prima Aulia Gusta <primaulia@gmail.com>
2017-06-27 18:40:32 +0800

    latest update

--- js/dom-create.js
+++ js/dom-create.js
@@ -1,41 +1,42 @@
-// UPDATING EXISTING ELEMENT
-// target the p element
-var p = document.querySelector('p')
+  // UPDATING EXISTING ELEMENT
+  // target the p element
+  var p = document.querySelector('p')
 
-// update the p textContent property
-// p.textContent = 'my new description'
+  // update the p textContent property
+  // p.textContent = 'my new description'
 
-// TARGET THE BUTTON
-var button = document.querySelector('button')
-button.addEventListener('click', addToCart)
+  // TARGET THE BUTTON
+  var button = document.querySelector('button')
+  button.addEventListener('click', addToCart)
 
-function addToCart () {
-  var shoppingList = document.querySelectorAll('#shopping-list li')
-  if (!shoppingList.length) {
-    alert('shopping list is empty')
-    return // need to return, so the function stops running
+  function addToCart () {
+    var shoppingList = document.querySelectorAll('#shopping-list li')
+    if (!shoppingList.length) {
+      alert('shopping list is empty')
+      return // need to return, so the function stops running
+    }
+
+    var cartList = document.querySelector('#cart-list')
+    var lastItem = shoppingList[shoppingList.length - 1]
+    cartList.appendChild(lastItem)
   }
 
-  var cartList = document.querySelector('#cart-list')
-  var lastItem = shoppingList[shoppingList.length - 1]
-  cartList.appendChild(lastItem)
-}
+  // newListText is the input
+  function ulCreator (newListText) {
+    var newLi = document.createElement('li')
+    newLi.textContent = newListText
 
-// newListText is the input
-function ulCreator (newListText) {
-  var newLi = document.createElement('li')
-  newLi.textContent = newListText
+    var ul = document.querySelector('ul')
+    ul.appendChild(newLi)
+  }
 
-  var ul = document.querySelector('ul')
-  ul.appendChild(newLi)
-}
-
-function paraCreator () {
-  // CREATING NEW ELEMENT
-  // create new paragraph
-  var newP = document.createElement('p')
-  newP.textContent = 'new paragraphzzzz'
-  // target the parent
-  var body = document.body
-  body.appendChild(newP)
-}
+  function paraCreator () {
+    // CREATING NEW ELEMENT
+    // create new paragraph
+    var newP = document.createElement('p')
+    newP.textContent = 'new paragraphzzzz'
+    // target the parent
+    var body = document.body
+    body.appendChild(newP)
+  }
+})
---
+++ js/dom-google-shopping.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // THIS LINE STARTS THE PARSING OF THE JSON
+  var json = {}
+
+  fetch('../products.json')
+  .then(res => res.json())
+  .then((data) => {
+    // DO NOT REMOVE ANYTHING BEFORE THIS LINE
+    console.log('Checkout this JSON! ', data)
+
+    // START YOUR CODE FROM HERE AND HERE ONLY
+    // ALL YOUR INTERNAL FUNCTIONS SHOULD START FROM HERE AND HERE ONLY TOO
+
+    var items = data.items
+    var shoppingList = document.querySelector('#shopping-list')
+
+    // Starter code. List out items' name into the shopping list
+    // HINT: EVERY FUNCTIONS HERE WILL BE ABLE TO ACCESS THE items VARIABLE
+    items.forEach(function (item) {
+      var listItem = document.createElement('li')
+      listItem.textContent = item.product.title
+      shoppingList.appendChild(listItem)
+    })
+
+    // DO NOT REMOVE ANYTHING AFTER THIS LINE
+  })
+})
