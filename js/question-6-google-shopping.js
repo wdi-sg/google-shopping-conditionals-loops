eb9a3e715b97bb2332dfb4b682109c2079d99397
Dominic Phua <dominikphua@gmail.com>
2017-06-27 10:04:37 +0800

    Bug on question 6

--- js/question-6-google-shopping.js
+++ js/question-6-google-shopping.js
@@ -6,3 +6,5 @@ for (var i = 0; i < data.items.length; i++) {
   console.log('Link: ' + data.items[i].product.images[0])
   console.log('')
 }
+
+// console.log(data.items[0].product.images[0].string[0])
