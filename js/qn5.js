3aa52798faa296e4ba1ddf164cf33c2868b252ff
Nur Hidayah <dayah@Seths-MacBook.local>
2017-09-24 22:29:39 +0800

    qn 5 - used && here

--- js/qn5.js
+++ js/qn5.js
@@ -7,11 +7,13 @@ var items = data.items
 
 //go through every single item
 for (var counter = 0; counter <items.length; counter ++) {
-  //go through the item>product>brand (need to open this)
-  //if the title> brand has "Cannon"
-  // print the title of it
   var item = items[counter]
-  var title = item.product.title //all the products that you have
+  var brand = item.product.brand
+  var author = item.product.author.name//all the products that you have
+  //console.log(brand)
   // var images = item.product.images
   // var availability = item.product.inventories[0].availability
+  if (brand === 'Canon' && author === 'eBay'){
+    console.log(item.product.title, author)
+  }
 }
