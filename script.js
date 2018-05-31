dccdb389735f4c1ca1e45c836fd4afa0b8fdbd7f
Julian Poh <julianpoh01@gmail.com>
2018-05-31 17:20:21 +0800

    Completed part 1

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
+// Write your solutions below
+
+// Go through the items and find all results that have kind of shopping#product. 
+
+// Print the count of these results. 
+var shop = products.items;
+var count = 0;
+
+for (i=0; i<shop.length; i++) {
+    if (shop[i].kind == "shopping#product") {
+        count++;
+    }
+}
+
+console.log(count);
+
+// Where else is this count information stored in the search results?
 
 
-//Go through the items and find all results that have kind of shopping#product. Print the count of these results. Where else is this count information stored in the search results?
+// Print the title of all items with a backorder availability in inventories.
 
 
-//Print the title of all items with a backorder availability in inventories.
+// Print the title of all items with more than one image link.
 
 
-//Print the title of all items with more than one image link.
+// Print all "Canon" products in the items (careful with case sensitivity).
 
 
-//Print all "Canon" products in the items (careful with case sensitivity).
+// Print all items that have an author name of "eBay" and are brand "Canon".
 
 
-//Print all items that have an author name of "eBay" and are brand "Canon".
-
-
-//Print all the products with their brand, price, and an image link.
+// Print all the products with their brand, price, and an image link.
