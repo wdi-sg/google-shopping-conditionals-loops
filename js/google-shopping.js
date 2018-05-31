010d4870db06b7a87e54511ff9be71c668ad4efb
Julian Poh <julianpoh01@gmail.com>
2018-05-31 15:43:55 +0800

    updated google shopping.js

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -1,3 +1,26 @@
 // Write your solutions below
 
 console.log(products);
+
+// Go through the items and find all results that have kind of shopping#product. 
+
+
+// Print the count of these results. 
+
+
+// Where else is this count information stored in the search results?
+
+
+// Print the title of all items with a backorder availability in inventories.
+
+
+// Print the title of all items with more than one image link.
+
+
+// Print all "Canon" products in the items (careful with case sensitivity).
+
+
+// Print all items that have an author name of "eBay" and are brand "Canon".
+
+
+// Print all the products with their brand, price, and an image link.
