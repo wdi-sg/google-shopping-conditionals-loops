6d1bdb8b3890b2677d6cd1a60f63c8d5b5e3b7c0
sadakosa <sabrinachow98@gmail.com>
2018-05-31 16:54:58 +0800

    with HTML

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -105,6 +105,25 @@ for (var b=0; b<brand.length; b++) {
 	console.log("brand: " + brand[b] + "(" + (b+1) + ")");
 	console.log("price: " + price[b] + "(" + (b+1) + ")");
 	console.log('image: ' + img[b] + "(" + (b+1) + ")");
+
+	var div = document.createElement("div");
+	var ne = document.createElement('h1');
+	var br = document.createElement('p');
+	var pr = document.createElement('p');
+	var im = document.createElement('p');
+
+	ne.appendChild(document.createTextNode('Item: ' + (b+1)));
+	br.appendChild(document.createTextNode("brand: " + brand[b]));
+	pr.appendChild(document.createTextNode("price: " + price[b]));
+	im.appendChild(document.createTextNode('image: ' + img[b]));
+
+	div.appendChild(ne);	
+	div.appendChild(br);
+	div.appendChild(pr);
+	div.appendChild(im);
+
+	var element = document.getElementById("div");
+	element.appendChild(div);
 };
 
 
