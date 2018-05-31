6a6c35bfd0bf9039b7d513d95b842593c5c21f5e
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-05-31 18:22:24 +0800

    question 2 and 3 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//1.) Go through the `items` and find all results that have `kind` of `shopping#product`. = 25
+//1.) Go through the `items` and find all results 
+//that have `kind` of `shopping#product`. = 25
 //Print the count of these results. = 25
 
 var count = 0;
@@ -13,3 +14,33 @@ for (var i = 0 ; i < products.items.length; i ++) {
 //Where else is this count information stored in the search results?
 //"itemsPerPage": 25,
 //"currentItemCount": 25,
+
+
+//2.) Print the `title` of all items with a
+// `backorder` availability in `inventories`.
+
+
+for (var i = 0 ; i < products.items.length; i ++) {
+	if (products.items[i].product.inventories[0].availability == "backorder") {
+		console.log(products.items[i].product.title)
+	}
+}
+
+
+//3.) Print the `title` of all items with more than one image link
+
+
+for (var i = 0 ; i < products.items.length; i ++) {
+	if (products.items[i].product.images.length > 1) {
+		console.log(products.items[i].product.title)
+	}
+}
+
+//4.) Print all "Canon" products in the items 
+//(careful with case sensitivity).
+
+//5.) Print all `items` that have an author name of "eBay" 
+//and are brand "Canon".
+
+//6.) Print all the products with their
+// **brand**, **price**, and an **image link**
