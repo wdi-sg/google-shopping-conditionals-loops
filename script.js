a5da0b58d0ab60b2eb2719200cd8cef87df07a81
Julian Poh <julianpoh01@gmail.com>
2018-06-01 15:40:31 +0800

    made corrections to q2-5

--- script.js
+++ script.js
@@ -15,63 +15,39 @@ for (i=0; i<shop.length; i++) {
 console.log(count);
 
 // Where else is this count information stored in the search results?
-console.log("I have no idea.");
+  
 
 
 // Print the title of all items with a backorder availability in inventories.
-var backOrders = [];
-
-for (var i=0, len=products.length; i<len; i++) {
-  for (var j=0, len2=items[i].length; j<len2; j++) {
-    for (var k=0, len3=inventories[j].length; k<len3; k++) {
-       if (inventories[j].availability == "backorder") {
-        backOrders.push(items[i].title);
-      }
-    }
+for (var i=0; i<shop.length; i++); {
+  if (shop[i].product.inventories[0].availability === "backorder"); {
+    console.log(shop[i].product.title);
   }
 }
-console.log(backOrders);
 
 // Print the title of all items with more than one image link.
-for (var i=0, len=products.length; i<len; i++) {
-  for (var j=0, len2=items[i].length; j<len2; j++) {
-    for (var k=0, len3=images[j].length; k<len3; k++) {
-       if ([j].length > 1) {
-        }
-     }
-   }
- }
-console.log([i].title);
+
 
 // Print all "Canon" products in the items (careful with case sensitivity).
-var can = products.items;
-var canonPdts = [];
-
-for (i=0; i<can.length; i++) {
- if (can[i].brand == "Canon") {
-  canonPdts.push([i].title);
- }
+for (var i=0; i<shop.length; i++) {
+  if (shop[i].product.brand.toLowerCase() === "canon"); {
+    console.log(shop[i].product.title);
+  }
 }
-console.log(canonPdts);
-
-
+  
 // Print all items that have an author name of "eBay" and are brand "Canon".
-var can = products.items;
-var canonEbay = [];
-
-for (i=0; i<can.length; i++) {
- if (can[i].brand == "Canon" && can[i].author == "eBay") {
-  canonEbay.push([i].title);
- }
+//look for other methods to find ebay besides '.include', eg. indexOf.
+for (i=0; i<shop.length; i++) {
+ if (shop[i].product.brand.toLowerCase() === "canon" && shop[i].product.author.toLowerCase().include("ebay")) {
+  console.log(shop[i].product.title);
+  }
 }
-console.log(canonEbay);
 
 // Print all the products with their brand, price, and an image link.
-
-var brand = [];
-var price = [];
-var imgLink = [];
-
-for (var i=0; i<products.length; i++) {
-  console.log("Brand: " + brand + ", Price: $" + price + ", Image: " + imgLink);
+function getByBrands(brand) {
+  for (i=0; i<shop.length; i++) {
+    console.log(shop[i].product.brand);
+    console.log(shop[i].product.inventories[0].price);
+    console.log(shop[i].images[0]); 
+  }
 }
