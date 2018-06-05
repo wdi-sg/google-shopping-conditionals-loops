0a3b7be18546880f80c3a800bc05ab7dbba39fb3
hygro5d <faidhiamran@Faidhis-MacBook-Pro.local>
2018-06-05 10:10:37 +0800

    half

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -1,3 +1,28 @@
-// Write your solutions below
 
-console.log(products);
+//PART 1
+console.log("Total number of shopping products is " + products.items.length);
+
+
+//PART 2
+var productsItems = products.items
+
+function backorderavailability(x) {
+ for (var i =0; i < (x).length; i++) {
+  if (x[i].product.inventories[0].availability == "backorder")
+  console.log(x[i].product.title); }
+}
+
+
+console.log(backorderavailability(productsItems));
+
+
+//PART 3
+var productsItems = products.items
+
+function imagelink(x) {
+ for( var i =0; i < (x).length; i++)	{
+   if (x[i].product.images.length > 1)
+   console.log(x[i].product.title); }
+}
+
+console.log(imagelink(productsItems));
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-console.log('Part 1');
+//part1
+var counter = products.items.length;
+console.log(counter);
 
-var count = 0;
-var getItemsByBrand = function() {
-	for (var i = 0; i < products.items.length; (i++) {
-		if (products.items[i].kind === "shopping#product") {
-			count += 1;		
-		}
-	} console.log(count);
-}
+console.log(products.itemsPerPage);
+console.log(products.currentItemCount);
 
-shopping();
-console.log('')
+//part2
+for (var i = 0; i < 25; i++) {
+  if (products.items[i].product.inventories[0].availability === "backorder")
+    console.log("This item " + products.items[i].product.title + " currenty unavailable.");
 
+//part3
+for (var k = 0; k < 25; k++) {
+  if (products.items[k].product.images.length > 2)
+    console.log("This item " + products.items[k].product.title + " has more than 1 image.");
