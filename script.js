8b88a5f7d8914d99e2a21df258b98fe19e9c83a5
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-09-05 22:29:49 +0800

    Modify all object notations to be in variables

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 //Go through the items and find all results that have kind of shopping#product.
 //Print the count of these results.
 //Where else is this count information stored in the //search results?
-//products.items.currentItemCount
+//items.currentItemCount
 
 var count = 0;
+var items = products.items;
+var inventories, availability;
+var brand, condition, title, name, imageLink, price;
+var userPref, userBrand, userCondition;
 
-for (var i = 0; i < products.items.length; i++) {
-  if (products.items[i].kind === 'shopping#product') {
+for (var i = 0; i < items.length; i++) {
+
+  if (items[i].kind === 'shopping#product') {
     count ++;
   };
 };
 
-console.log('');
-console.log(`1. There are ${count} shopping#product items.`);
+console.log (`1. There are ${count} shopping#product items.`);
 
 //Print the title of all items with a backorder availability in inventories.
 
-console.log('');
-console.log('2. Products on back order:');
+console.log ('');
+console.log ('2. Products on back order:');
 
-for(var i = 0; i < products.items.length; i++) {
-  for (var j = 0; j < products.items[i].product.inventories.length; j++) {
-    if (products.items[i].product.inventories[j].availability === 'backorder') {
-      console.log(products.items[i].product.title)
+for (var i = 0; i < items.length; i++) {
+  inventories = items[i].product.inventories;
+
+  for (var j = 0; j < inventories.length; j++) {
+    availability = inventories[j].availability;
+    title = items[i].product.title;
+
+    if (availability === 'backorder') {
+      console.log(title);
     }
   }
 }
@@ -32,9 +41,10 @@ for(var i = 0; i < products.items.length; i++) {
 console.log('');
 console.log('3. Products with more than 1 image:');
 
-for(var i = 0; i < products.items.length; i++) {
-  if (products.items[i].product.images.length > 1) {
-    console.log(products.items[i].product.title);
+for (var i = 0; i < items.length; i++) {
+
+  if (items[i].product.images.length > 1) {
+    console.log(items[i].product.title);
   }
 }
 
@@ -43,10 +53,12 @@ for(var i = 0; i < products.items.length; i++) {
 console.log('');
 console.log('4. Products of Canon Brand:');
 
-for(var i = 0; i < products.items.length; i++) {
-  //console.log(products.items[i].product.brand);
-  if (products.items[i].product.brand === 'Canon') {
-    console.log(products.items[i].product.title);
+for (var i = 0; i < items.length; i++) {
+  brand = items[i].product.brand;
+  title = items[i].product.title;
+
+  if (brand === 'Canon') {
+    console.log(title);
   }
 }
 
@@ -55,9 +67,13 @@ for(var i = 0; i < products.items.length; i++) {
 console.log('');
 console.log('5. Canon Products from eBay:');
 
-for(var i = 0; i < products.items.length; i++) {
-  if (products.items[i].product.brand === 'Canon' && products.items[i].product.author.name.indexOf('eBay') !== -1) {
-    console.log(products.items[i].product.title);
+for (var i = 0; i < items.length; i++) {
+  brand = items[i].product.brand;
+  name = items[i].product.author.name;
+  title = items[i].product.title;
+
+  if (brand === 'Canon' && name.indexOf('eBay') !== -1) {
+    console.log(title);
   }
 }
 
@@ -65,12 +81,14 @@ for(var i = 0; i < products.items.length; i++) {
 
 console.log('');
 console.log('6. Products Listing:');
-for(i in products.items) {
-  var brand = products.items[i].product.brand;
-  var imageLink = products.items[i].product.images[0].link;
-  for (j in products.items[i].product.inventories) {
-    var price = products.items[i].product.inventories[j].price;
+for (i in items) {
+  brand = items[i].product.brand;
+  imageLink = items[i].product.images[0].link;
+
+  for (j in items[i].product.inventories) {
+    price = items[i].product.inventories[j].price;
   }
+
   console.log(`Brand: ${brand}, Price: ${price}, Image: ${imageLink}`);
 }
 
@@ -81,31 +99,39 @@ for(i in products.items) {
 //Prompt the user if they want to see only new or used items.
 //Prompt the user what kind of search they want to do- search by brand or search by condition.
 
-var userPref = prompt('Do you want search by item brand or condition?', 'brand or condition').toLowerCase();
+userPref = prompt('Do you want search by item brand or condition?', 'brand or condition').toLowerCase();
 
 //Then prompt the user to put in ther actual search value- (new/used for condition or brand name for brand)
 
 if (userPref === 'brand')
-  var userBrand = prompt('What brand are you looking for?').toLowerCase();
+  userBrand = prompt('What brand are you looking for?').toLowerCase();
 else if (userPref === 'condition')
-  var userCondition = prompt('Do you prefer new or used?', 'new or used').toLowerCase();
+  userCondition = prompt('Do you prefer new or used?', 'new or used').toLowerCase();
+else
+  userPref = 'error';
 
 console.log('')
 console.log('User search results:')
 
 count = 0
 
-for(i in products.items){
-  var brand = products.items[i].product.brand;
-  var condition = products.items[i].product.condition;
+for (i in items) {
+
+  brand = items[i].product.brand;
+  condition = items[i].product.condition;
+  title = items[i].product.title;
+
   if ((brand.toLowerCase().indexOf(userBrand) !== -1) || (condition.toLowerCase().indexOf(userCondition) !== -1)) {
-    console.log(products.items[i].product.title);
+    console.log(title);
     count++;
   }
 }
 
 //Print out some special error text (e.g. "Sorry, nothing found") if there were no results.
-if (count <= 0)
+
+if (count <= 0 && userPref !== 'error')
   console.log("Sorry, nothing found");
+else if (userPref === 'error')
+  console.log('Invalid input detected!')
 else
   console.log(`Total ${count} results.`)
