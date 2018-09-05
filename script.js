17e45e0b4164db23bf1afffe3a1d715005c89741
weiwenl <weiwenl.justin@gmail.com>
2018-09-06 00:29:08 +0800

    Part5

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ for(var i=0; i < products.items.length; i++){
   //Print all 25 items
   /*console.log(products.items[i]);
 
-  //Find conditions of 'kind' = 'shopping#product'
+  //Find conditions of kind = 'shopping#product'
   if(products.items.kind === "shopping#product"){
 
     //For every match, count will be 1
@@ -41,8 +41,18 @@ for(var i=0; i < products.items.length; i++){
 }*/
 
 //Q4
-  //Find condition for all "Canon".lowercase item
-  if(products.items[i].product.brand.toLowerCase() === "canon"){
+  //Find condition for all 'Canon'.lowercase item
+  /*if(products.items[i].product.brand.toLowerCase() === "canon"){
+
+    //Print product title of identified match
+    console.log(products.items[i].product.title);
+  }
+}*/
+
+//Q5
+  //Find condition for all author == 'eBay' && brand == 'canon'
+  if(products.items[i].product.author.name.includes("eBay") &&
+        products.items[i].product.brand.toLowerCase() === "canon"){
 
     //Print product title of identified match
     console.log(products.items[i].product.title);
