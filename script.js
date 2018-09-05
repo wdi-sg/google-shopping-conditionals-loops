d0e1c8e66f4a84811790e27bbc63a61f4f985bd0
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-09-05 17:34:44 +0800

    Complete third deliverable

--- script.js
+++ script.js
@@ -20,11 +20,22 @@ console.log(products.items[0]['product']['inventories'][0]['availability']);
 console.log(products.items[0]['product']['title']);
 console.log(products.items[0]['product']['inventories'].length);
 */
+console.log('Products on back order:');
 
 for(var i = 0; i < products.items.length; i++) {
   for (var j = 0; j < products.items[i]['product']['inventories'].length; j++) {
     if (products.items[i]['product']['inventories'][j]['availability'] === 'backorder') {
       console.log(products.items[i]['product']['title'])
     }
+  }
+}
+
+//Print the title of all items with more than one image link.
+
+console.log('Products with more than 1 image:')
+
+for(var i = 0; i < products.items.length; i++) {
+  if (products.items[i]['product']['images'].length > 1) {
+    console.log(products.items[i]['product']['title']);
   }
 }
