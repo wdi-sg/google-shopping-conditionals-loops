0a0a4b5f537da95869b6dc45a557c13467e5dba6
mervyns <mervyns@gmail.com>
2018-09-05 17:48:30 +0800

    part 2

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 console.log(products)
 // Part 1
-var count = 0
+// var count = 0
+// for (var i = 0; i < products.items.length; i++) {
+//   if (products.items[i].kind === "shopping#product") {
+//     console.log(products.items[i].kind);
+//     count++;
+//   }
+// };
+// console.log(count)
+
+//Part 2
 for (var i = 0; i < products.items.length; i++) {
-  if (products.items[i].kind === "shopping#product") {
-    console.log(products.items[i].kind);
-    count++;
+  if (products.items[i].product.inventories[0].availability === "backorder") {
+    console.log(products.items[i].product.title);
   }
 };
-console.log(count)
