2e343bc26425482758b0fa82b2d140264121e653
lyvzm <chanleyou@gmail.com>
2018-09-05 22:07:50 +0800

    combined print into functions and cleaned up code

--- script.js
+++ script.js
@@ -1,83 +1,89 @@
-// count shopping#product
+var items = products.items;
+
+var printTitle = function(i) {
+  console.log(items[i].product.title);
+}
+
+var printItem = function (i) {
+  printTitle(i);
+  console.log("Brand: " + items[i].product.brand);
+  console.log("Price: $" + items[i].product.inventories[0].price);
+  console.log("Image: " + items[i].product.images[0].link);
+}
+
+/*
+
+// Go through the items and find all results that have kind of shopping#product. Print the count of these results.
 var shoppingProductsCount = 0;
 
-for (var i = 0; i < products.items.length; i++) {
-  if (products.items[i].kind === "shopping#product") {
+for (var i = 0; i < items.length; i++) {
+  if (items[i].kind === "shopping#product") {
     shoppingProductsCount ++;
   }
 }
-
 console.log("No. of shopping products: " + shoppingProductsCount);
 
-//print title of items with availability of backorder
-for (var i = 0; i < products.items.length; i++) {
-  if (products.items[i].product.inventories[0].availability === "backorder") {
-    console.log(products.items[i].product.title + " has an availability of backorder.");
+
+//Print the title of all items with a backorder availability in inventories.
+for (var i = 0; i < items.length; i++) {
+  if (items[i].product.inventories[0].availability === "backorder") {
+    printTitle(i);
   }
 }
 
-//print title of items with more than one image link
-for (var i = 0; i < products.items.length; i++) {
-  if (products.items[i].product.images.length > 1) {
-    console.log(products.items[i].product.title + " has more than one image link.");
+//Print the title of all items with more than one image link.
+for (var i = 0; i < items.length; i++) {
+  if (items[i].product.images.length > 1) {
+    printTitle(i);
   }
 }
 
-//print all "Canon" products
-for (var i = 0; i < products.items.length; i++) {
-  var brand = products.items[i].product.brand;
-  if (brand.toLowerCase() === "canon") {
-    console.log(products.items[i].product.title + " is a Canon product.");
+//Print all "Canon" products in the items (careful with case sensitivity).
+for (var i = 0; i < items.length; i++) {
+  if (items[i].product.brand.toLowerCase() === "canon") {
+    printTitle(i);
   }
 }
 
-//print all items that have an author name of "eBay" and are brand "Canon"
-for (var i = 0; i < products.items.length; i++) {
-  var brand = products.items[i].product.brand;
-  var author = products.items[i].product.author.name;
-  if (brand.toLowerCase() === "canon" && author.toLowerCase().includes("ebay") === true) {
-    console.log(products.items[i].product.title + " is a Canon product from eBay.");
+//Print all items that have an author name of "eBay" and are brand "Canon".
+for (var i = 0; i < items.length; i++) {
+  if (items[i].product.brand.toLowerCase() === "canon" && items[i].product.author.name.toLowerCase().includes("ebay")) {
+    printTitle(i);
   }
 }
 
 //print all products with their brand, price and an image link
-var printItem = function (i) {
-  console.log("Number: " + (i + 1));
-  console.log("Title: " + products.items[i].product.title);
-  console.log("Brand: " + products.items[i].product.brand);
-  console.log("Price: " + products.items[i].product.inventories[0].price);
-  console.log("Image Link: " + products.items[i].product.images[0].link);
-}
-
-for (var i = 0; i < products.items.length; i++) {
+for (var i = 0; i < items.length; i++) {
   printItem (i);
 }
 
-//prompt the user for the product brand and print only these products
-//prompt the user to show only new or used products
-//prompt the user to search by brand or condition
-//then prompt the user to put in their actual search condition
-//print out some special error text if there were no results
+*/
+
+//Prompt the user for the product brand and print only those products.
+//Prompt the user if they want to see only new or used items.
+//Prompt the user what kind of search they want to do- search by brand or search by condition.
+//Then prompt the user to put in ther actual search value- (new/used for condition or brand name for brand)
+//Print out some special error text (e.g. "Sorry, nothing found") if there were no results.
+
+var productsFound = 0;
 
 var searchBool = confirm("Do you want to search for a product by brand or condition?");
 
-if (searchBool === true) {
+if (searchBool) {
 
   var brandOrCondition;
-  var productsFound = 0;
 
   do {
-    brandOrCondition = prompt("Ok. Search by brand or condition? (no to cancel)").toLowerCase();
-  } while (brandOrCondition !== "brand" && brandOrCondition !== "condition" && brandOrCondition !== "no");
+    brandOrCondition = prompt("Ok. Search by brand or condition?").toLowerCase();
+  } while (brandOrCondition !== "brand" && brandOrCondition !== "condition");
 
   if (brandOrCondition === "brand") {
 
-    var productBrand = prompt("What brand are you searching for?");
+    var searchBrand = prompt("What brand are you searching for?");
+    console.log("Ok. Looking for '" + searchBrand + "' products.");
 
-    console.log("Ok. Looking for '" + productBrand + "' products.");
-
-    for (var i = 0; i < products.items.length; i++) {
-      if (products.items[i].product.brand.toLowerCase() === productBrand.toLowerCase()) {
+    for (var i = 0; i < items.length; i++) {
+      if (items[i].product.brand.toLowerCase() === searchBrand.toLowerCase()) {
         printItem (i);
         productsFound ++;
       }
@@ -91,24 +97,34 @@ if (searchBool === true) {
     } while (newOrUsed !== "new" && newOrUsed !== "used");
 
     if (newOrUsed === "new") {
-      for (var i =0; i < products.items.length; i++) {
-        if (products.items[i].product.condition === "new") {
+      for (var i =0; i < items.length; i++) {
+        if (items[i].product.condition === "new") {
           printItem (i);
           productsFound ++;
         }
       }
     } else if (newOrUsed === "used") {
-      for (var i =0; i < products.items.length; i++) {
-        if (products.items[i].product.condition === "used") {
+      for (var i =0; i < items.length; i++) {
+        if (items[i].product.condition === "used") {
           printItem (i);
           productsFound ++;
         }
       }
     }
   }
+} else {
+  console.log("Ok. Listing all results.");
 
-  if (productsFound === 0) {
-    console.log("Sorry! Nothing found.");
+  for (var i = 0; i < items.length; i++) {
+    printItem (i);
+    productsFound ++;
   }
+}
 
+if (productsFound > 1) {
+  console.log(productsFound + " products found.");
+} else if (productsFound === 1) {
+  console.log("1 product found.")
+} else if (productsFound === 0) {
+  console.log("Sorry! Nothing found.");
 }
