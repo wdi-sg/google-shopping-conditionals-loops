11d3c49a3a1a0f421ac65949c68fd88f2051e5bf
shroomcrumpet <kencheng3@gmail.com>
2018-09-05 18:37:37 +0800

    Part 2

--- script.js
+++ script.js
@@ -16,15 +16,58 @@ console.log(products);
 //  }
 // }
 
-var count = 0;
+
+// Part 1 // var count = 0;
+// Part 1 //
+// Part 1 // for ( var i = 0; i < products.items.length; i++ ) {
+// Part 1 //
+// Part 1 //   var item = products.items[i];
+// Part 1 //
+// Part 1 //   if ( item.kind === "shopping#product") {
+// Part 1 //       count++;
+// Part 1 //   }
+// Part 1 // }
+// Part 1 //
+// Part 1 // console.log(`Total count of shopping#product is ${count}.`)
+
+
+// Part 2 // Print the title of all items with a backorder availability in inventories.
+
+var count = 0
 
 for ( var i = 0; i < products.items.length; i++ ) {
 
   var item = products.items[i];
 
-  if ( item.kind === "shopping#product") {
-      count++;
+  if ( item.product.inventories[0].availability === "backorder") {
+
+      console.log(`${item.product.title} is backordered.`)
   }
 }
 
-console.log(`Total count of shopping#product is ${count}.`)
+// Part 3 // Print the title of all items with more than one image link.
+
+// Part 4 // Print all "Canon" products in the items (careful with case sensitivity).
+
+// Part 5 // Print all items that have an author name of "eBay" and are brand "Canon".
+
+// Part 6 // Print all the products with their brand, price, and an image link
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
