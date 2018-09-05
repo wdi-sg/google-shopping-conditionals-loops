e4fea5236c7bf2ed57f5720e955a6070c6a241b5
mervyns <mervyns@gmail.com>
2018-09-05 17:53:27 +0800

    part 3

--- script.js
+++ script.js
@@ -10,8 +10,15 @@ console.log(products)
 // console.log(count)
 
 //Part 2
+// for (var i = 0; i < products.items.length; i++) {
+//   if (products.items[i].product.inventories[0].availability === "backorder") {
+//     console.log(products.items[i].product.title);
+//   }
+// };
+
+// Part 3
 for (var i = 0; i < products.items.length; i++) {
-  if (products.items[i].product.inventories[0].availability === "backorder") {
-    console.log(products.items[i].product.title);
-  }
+	if (products.items[i].product.images.length > 1) {
+		console.log(products.items[i].product.title)
+	}
 };
