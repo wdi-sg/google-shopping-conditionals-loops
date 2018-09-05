25b7a715eb5626ace43753b2df4b3d9d9fef8bfa
ah5ive <ah5ive@gmail.com>
2018-09-05 18:32:30 +0800

    finish Count shopping kind items for shopping#product

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Halo World!");
+console.log(products);
+
+// Go through the items and find all results that have kind of shopping#product.
+// Print the count of these results. Where else is this count information
+// stored in the search results?
+// var myKind = "shopping#products";
+
+for( var i = 0; i<products.items.length; i++){
+  console.log( products.items[i].kind);
+}
+//   var item = products.items[i]
+//   console.log( item.kind);
+//   console.log( item );
+
+// if( item.id === myKind ){
+//     //console.log( "YAYAYAYA");
+//   }
+// }
+
+// for (var i = 0; i<products.items.length;i++) {
+
+// if (products.items[i].product.inventories[0].availability === "backorder"){
+//     console.log(products.items[i].product.title);
+//   }
+// }
+// for (var i = 0; i <= products.length; i++){
+// console.log(products.items[i].kind.product);
+// }
