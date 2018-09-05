d71e619f0b7d0d1ac9fd1c6a0186ea6e5873849c
saufm68 <saufm68@gmail.com>
2018-09-05 22:10:41 +0800

    completed the last further

--- script.js
+++ script.js
@@ -74,7 +74,9 @@ Prompt the user for the product brand and print only those products.
 Prompt the user if they want to see only new or used items.*/
 
 // var inputBrand = prompt("Enter a brand");
+// var inputBrandEdited = inputBrand[0].toUpperCase() + inputBrand.substring(1).toLowerCase();
 // var inputCondition = prompt("Do you prefer 'new' or 'used'");
+// var inputConditionEdited = inputCondition.toLowerCase();
 
 
 // for (var i = 0; i < items.length; i++ ) {
@@ -85,7 +87,7 @@ Prompt the user if they want to see only new or used items.*/
 //   var randomImglink = Math.floor(Math.random() * itemImage.length);
 //   var condition = items[i].product.condition;
 
-//   if ( brand === inputBrand && condition === inputCondition) {
+//   if ( brand === inputBrandEdited && condition === inputConditionEdited) {
 //     console.log(`Brand: ${brand} \n Price: ${price} \n Condition: ${condition} \n Image: ${itemImage[randomImglink].link}`);
 //   }
 // }
@@ -93,55 +95,51 @@ Prompt the user if they want to see only new or used items.*/
 /* Further
 Prompt the user what kind of search they want to do- search by brand or search by condition.
 
-Then prompt the user to put in ther actual search value- (new/used for condition or brand name for brand)*/
+Then prompt the user to put in ther actual search value- (new/used for condition or brand name for brand)
 
-var inputArray = [];
+Print out some special error text (e.g. "Sorry, nothing found") if there were no results.*/
 
+var brandsArray = [];
+var conditionArray = [];
 
 var prompt1 = prompt("Would you prefer to search by 'brand' or 'condition");
+var promptEdited = prompt1.toLowerCase();
 
 
-if (prompt1 === "brand") {
-  input = prompt("Enter a brand");
-  inputArray.push(input);
-} else if (prompt1 === "condition") {
-  input = prompt("Do you prefer 'new' or 'used'");
-  inputArray.push(input);
+if (promptEdited === "brand") {
+  var input = prompt("Enter a brand");
+  var inputEdited = input[0].toUpperCase() + input.substring(1).toLowerCase();
+} else if (promptEdited === "condition") {
+  var input = prompt("Do you prefer 'new' or 'used'");
+  var inputEdited = input.toLowerCase();
 }
 
-for (var i = 0; i < items.length; i++ ) {
+for (var i = 0; i < items.length; i++) {
 
   var brand =  items[i].product.brand;
+  brandsArray.push(brand);
   var price = items[i].product.inventories[0].price
   var itemImage = items[i].product.images;
   var randomImglink = Math.floor(Math.random() * itemImage.length);
   var condition = items[i].product.condition;
+  conditionArray.push(condition);
 
-  if (brand === inputArray[0] || condition === inputArray[0] ) {
+
+  if (brand === inputEdited || condition === inputEdited ) {
     console.log(`Brand: ${brand} \n Price: ${price} \n Condition: ${condition} \n Image: ${itemImage[randomImglink].link}`);
   }
 
 }
 
-// if ( brand !== inputArray[0] || condition !== inputArray[0]) {
-//   console.log("Sorry, nothing found")
-//   break;
-//   }
 
+if (!brandsArray.includes(inputEdited) && promptEdited === "brand") {
 
+  console.log("Sorry, nothing found");
 
+} else if (!conditionArray.includes(inputEdited) && promptEdited === "condition") {
 
+  console.log("Sorry, nothing found");
 
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
