fba96dfbd88a5660969e8498ab7125b1629c1fab
xnithunx <nithun.nanda@gmail.com>
2018-09-06 00:10:26 +0800

    Completed parts 2 to 6

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
-console.log(products);
 
-//var searchId = "tag:google.com,2010:shopping/products/6347724/17225849483606303723";
 
-var count = 0;
-for( var i=0; i<products.items.length; i++){
-  //console.log( products.items[i].id);
+//part 2 Print the title of all items with a backorder availability in inventories.
+for (var i = 0; i < products.items.length; i++) {
 
-  var item = products.items[i];
+  if (products.items[i].product.inventories[0].availability === "backorder") {
+     console.log(products.items[i].product.title);
+   }
+ }
 
-  //console.log( item.id );
-  //console.log( item );
+ //part 3 Print the title of all items with more than one image link.
 
-  if( item.kind === "shopping#product" ){
-    count++;
-    console.log(count);
+for (var i = 0; i < products.items.length; i++) {
 
+  if (products.items[i].product.images.length > 1) {
+
+    console.log(products.items[i].product.title);
   }
 
 }
+
+//part 4 Print all "Canon" products in the items (careful with case sensitivity).
+
+for (var i = 0; i < products.items.length; i++) {
+
+   if (products.items[i].product.brand === "Canon") {
+     console.log(products.items[i].product.title)
+   }
+ }
+
+ //part 5 Print all items that have an author name of "eBay" and are brand "Canon".
+
+for (var i = 0; i < products.items.length; i++) {
+
+  if ((products.items[i].product.author.name === "eBay") && (products.items[i].product.brand === "Canon"))
+
+    console.log(products.items[i].product.title)
+}
+
+
+//part 6 Print all the products with their brand, price, and an image link
+
+for (var i = 0; i < products.items.length; i++)  {
+
+ console.log(products.items[i].product.brand, products.items[i].product.inventories[0].price, products.items[i].product.images[0].link);
+}
+
+
+
+
+
+
