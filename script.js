e800545b571c06e207ebb2d382747b458e8de603
benghui <benghui@gmail.com>
2018-09-06 00:12:00 +0800

    part 2

--- script.js
+++ script.js
@@ -3,13 +3,31 @@
 
 //Go through the items and find all results that have kind of shopping#product. Print the count of these results. Where else is this count information stored in the search results?
 
-var itemsCount = 0;
+/*var itemsCount = 0;
 
 for (var i=0; i < products.items.length; i++){
     if(products.items[i].kind === "shopping#product"){
         itemsCount++
     }
 }
-    console.log(itemsCount)
+    console.log(itemsCount)*/
 
 //It is also stored in "itemsPerPage" and "currentItemCount"
+
+//Print the title of all items with a backorder availability in inventories.
+
+//Location of "backorder" is products.items.product.inventories.availability
+
+for (var i=0; i < products.items.length; i++){
+    if (products.items[i].product.inventories[0].availability === "backorder"){
+        console.log(products.items[i].product.title);
+    }
+}
+
+//Print the title of all items with more than one image link.
+
+//Print all "Canon" products in the items (careful with case sensitivity).
+
+//Print all items that have an author name of "eBay" and are brand "Canon".
+
+//Print all the products with their brand, price, and an image link
