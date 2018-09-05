c80485a43d925cc851b12471685d0def1bd382c9
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-09-05 17:22:13 +0800

    Complete second deliverable

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Go through the items and find all results that have kind of shopping#product.
 //Print the count of these results.
 //Where else is this count information stored in the //search results?
+//products.items.currentItemCount
 
 var count = 0;
 
@@ -11,3 +12,19 @@ for (var i = 0; i < products.items.length; i++) {
 };
 
 console.log(`There are ${count} shopping#product items.`);
+
+//Print the title of all items with a backorder availability in inventories.
+
+/*
+console.log(products.items[0]['product']['inventories'][0]['availability']);
+console.log(products.items[0]['product']['title']);
+console.log(products.items[0]['product']['inventories'].length);
+*/
+
+for(var i = 0; i < products.items.length; i++) {
+  for (var j = 0; j < products.items[i]['product']['inventories'].length; j++) {
+    if (products.items[i]['product']['inventories'][j]['availability'] === 'backorder') {
+      console.log(products.items[i]['product']['title'])
+    }
+  }
+}
