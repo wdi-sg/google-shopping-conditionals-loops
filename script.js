02749788cd4a3ae54ef7e610e8ac1a7d8c33e729
ah5ive <ah5ive@gmail.com>
2018-09-06 09:08:55 +0800

    Part 6 done but have problem with image list

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ for (var i = 0; i < products.items.length; i++){
   }
   end of part 4 */
 
-//start of part 5
+/*start of part 5
 for (var i = 0; i < products.items.length; i ++){
 
   if((products.items[i].product.author.name === "eBay") && (products.items[i].product.brand === "Canon")){
@@ -42,9 +42,26 @@ for (var i = 0; i < products.items.length; i ++){
   }
 
 }
-//end of part 5
+end of part 5 */
+// for (var i = 0; i < products.items.length; i ++ ){
 
+//   console.log(products.items[i]);
+// }
 
+//var brandId = "tag:google.com,2010:shopping/products/6347724/17225849483606303723";
+
+for( var i=0; i<products.items.length; i++){
+  //console.log( products.items[i].id);
+
+  var item = products.items[i];
+
+  console.log( "Brand:" +" " + (item.product.brand) + " " + "Price:"+ " "+(item.product.inventories[0].price) + " " + "Images" + " " +(item.product.images));
+  // console.log( item );\
+
+  // if( item.id === searchId ){
+
+  //   //console.log( "YAYAYAYA");
+  }
 
 
 
