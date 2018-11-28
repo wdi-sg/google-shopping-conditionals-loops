371a4aacd76e244682b4176c7eac7c61a5e8d297
Alan <alan88w@yahoo.com>
2018-11-28 20:06:26 +0800

    Uncomment code

--- script.js
+++ script.js
@@ -4,49 +4,49 @@ items = products.items;
 
 // Count the number of items that has kind = "shopping#product"
 
-// count = 0;
+count = 0;
 
-// for (var i = 0;i<items.length;i++) {
-//   item = items[i];
-//   if (item.kind = "shopping#product") {
-//     count++;
-//   }
-// }
+for (var i = 0;i<items.length;i++) {
+  item = items[i];
+  if (item.kind = "shopping#product") {
+    count++;
+  }
+}
 
 // console.log("Number of Shopping product is : "+count);
 
 // Print the title of all items with a backorder availability in inventories
 
-// for (var i = 0;i<items.length;i++) {
-//   item = items[i];
-//   if (item.product.inventories[0].availability = "backorder") {
-//     console.log(item.product.title);
-//   }
-// }
+for (var i = 0;i<items.length;i++) {
+  item = items[i];
+  if (item.product.inventories[0].availability = "backorder") {
+    console.log(item.product.title);
+  }
+}
 
 // Print the title of all items with more than one image link
-// for (var i = 0;i<items.length;i++) {
-//   item = items[i];
-//   if (item.product.images.length > 1) {
-//     console.log(item.product.title);
-//   }
-// }
+for (var i = 0;i<items.length;i++) {
+  item = items[i];
+  if (item.product.images.length > 1) {
+    console.log(item.product.title);
+  }
+}
 
 // Print all "Canon" products in the items (careful with case sensitivity).
-// for (var i = 0;i<items.length;i++) {
-//   item = items[i];
-//   if (item.product.brand.toLowerCase() === 'canon') {
-//     console.log(item.product.title);
-//   }
-// }
+for (var i = 0;i<items.length;i++) {
+  item = items[i];
+  if (item.product.brand.toLowerCase() === 'canon') {
+    console.log(item.product.title);
+  }
+}
 
 // Print all items that have an author name of "eBay" and are brand "Canon".
-// for (var i = 0;i<items.length;i++) {
-//   item = items[i];
-//   if (item.product.brand.toLowerCase() === 'canon' && item.product.author.name.toLowerCase() === "ebay") {
-//     console.log(item);
-//   }
-// }
+for (var i = 0;i<items.length;i++) {
+  item = items[i];
+  if (item.product.brand.toLowerCase() === 'canon' && item.product.author.name.toLowerCase() === "ebay") {
+    console.log(item);
+  }
+}
 
 
 // Print all the products with their brand, price, and an image link
