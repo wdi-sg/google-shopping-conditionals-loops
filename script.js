c91ebe88a86b0d029c038213bc3ad9e4360128b7
hongchiong <hongchiong@gmail.com>
2018-11-28 17:07:04 +0800

    print all Canon products

--- script.js
+++ script.js
@@ -7,15 +7,19 @@ for (var i in products.items) {
 console.log(count);
 
 for (var i in products.items) {
-    console.log(i);
     if (products.items[i].product.inventories[0].availability == "backorder") {
         console.log(products.items[i].product.title);
     }
 }
 
 for (var i in products.items) {
-    console.log(i);
     if (products.items[i].product.images.length > 1) {
+        console.log(products.items[i].product.title);
+    }
+}
+
+for (var i in products.items) {
+    if (products.items[i].product.brand == "Canon") {
         console.log(products.items[i].product.title);
     }
 }
