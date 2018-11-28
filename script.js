4ddeb6fe8f380a5f0ca84efba2194c8ee1df9d19
sean chan <sean@seans-MacBook-Air.local>
2018-11-28 21:31:25 +0800

    Complete further

--- script.js
+++ script.js
@@ -73,15 +73,108 @@
 
 //Print all the products with their brand, price, and an image link
 
-for(var i = 0; i < products.items.length; i++){
+// for(var i = 0; i < products.items.length; i++){
+//   var numItems = products.items[i];
+//   var itemBrand = numItems.product.brand;
+//   var itemPrice = numItems.product.inventories[0].price;
+//   var itemImage = numItems.product.images[0].link;
+//   // if(itemCanon.toLowerCase() === "canon" && itemAuth.toLowerCase().includes("ebay") === true) {
+//   //   console.log("Canon products from eBay:  " + numItems.product.title);
+//   // }
+//   console.log("Brand: " + itemBrand + "Price: " + itemPrice+ "Image: " + itemImage);
+//   }
+
+
+//##Further Part 1
+
+// Prompt the user for the product brand and print only those products.
+
+// Prompt the user if they want to see only new or used items.
+
+// var userInputBrand = prompt("Please insert product brand and print");
+// var userInputCon = prompt("Do you want to see new or used items");
+
+// var userBrand = userInputBrand;
+// var userCon = userInputCon;
+
+// for(var i = 0; i < products.items.length; i++){
+//   var numItems = products.items[i];
+//   var itemBrand = numItems.product.brand;
+//   var itemCond = numItems.product.condition;
+//   if(userBrand.toLowerCase() === itemBrand.toLowerCase() && userCon === itemCond.toLowerCase()){
+//     console.log(numItems.product.title);
+//   }
+//   }
+
+//##Further Part 2
+
+
+// Prompt the user what kind of search they want to do- search by brand or search by condition.
+
+// Then prompt the user to put in ther actual search value- (new/used for condition or brand name for brand)
+
+// var userInputSearch = prompt("Do you want to search by brand or condition");
+// var userFilter = userInputSearch.toLowerCase();
+
+// if(userFilter === "brand"){
+//   var userInputBrand = prompt("Please insert product brand and print");
+//   var userBrand = userInputBrand.toLowerCase();
+//   for(var i = 0; i < products.items.length; i++){
+//   var numItems = products.items[i];
+//   var itemBrand = numItems.product.brand;
+//   if(userBrand.toLowerCase() === itemBrand.toLowerCase()){
+//     console.log(numItems.product.title);
+//   }
+//   }
+// } else if(userFilter === "condition"){
+//   var userInputCon = prompt("Do you want to see new or used items");
+//   var userCon = userInputCon.toLowerCase();
+//   for(var i = 0; i < products.items.length; i++){
+//   var numItems = products.items[i];
+//   var itemCond = numItems.product.condition;
+//   if(userCon === itemCond.toLowerCase()){
+//     console.log(numItems.product.title);
+//   }
+//   }
+// } else {
+//   alert("Error!");
+// }
+
+//##Further Part 3
+
+// Print out some special error text (e.g. "Sorry, nothing found") if there were no results.
+
+var userInputSearch = prompt("Do you want to search by brand or condition");
+var userFilter = userInputSearch.toLowerCase();
+
+var count = 0;
+
+if(userFilter === "brand"){
+  var userInputBrand = prompt("Please insert product brand and print");
+  var userBrand = userInputBrand.toLowerCase();
+  for(var i = 0; i < products.items.length; i++){
   var numItems = products.items[i];
   var itemBrand = numItems.product.brand;
-  var itemPrice = numItems.product.inventories[0].price;
-  var itemImage = numItems.product.images[0].link;
-  // if(itemCanon.toLowerCase() === "canon" && itemAuth.toLowerCase().includes("ebay") === true) {
-  //   console.log("Canon products from eBay:  " + numItems.product.title);
-  // }
-  console.log("Brand: " + itemBrand + "Price: " + itemPrice+ "Image: " + itemImage);
+  if(userBrand.toLowerCase() === itemBrand.toLowerCase()){
+    console.log(numItems.product.title);
+    count++
   }
-
-
+  } if(count === 0){
+    console.log("Sorry, nothing found");
+  }
+} else if(userFilter === "condition"){
+  var userInputCon = prompt("Do you want to see new or used items");
+  var userCon = userInputCon.toLowerCase();
+  for(var i = 0; i < products.items.length; i++){
+  var numItems = products.items[i];
+  var itemCond = numItems.product.condition;
+  if(userCon === itemCond.toLowerCase()){
+    console.log(numItems.product.title);
+    count++;
+  }
+  } if(count === 0){
+    console.log("Sorry, nothing found");
+  }
+} else {
+  alert("Error!");
+}
