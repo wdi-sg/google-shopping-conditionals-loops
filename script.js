31e87a45c2c8f6acc5a62b23af3884766d1ac9cd
khyreerusydi <nicoraventbh@gmail.com>
2019-03-13 23:18:36 +0800

    created index.html & script.js, completed main deliverables

---
+++ script.js
@@ -0,0 +1,49 @@
+// console.log(products);
+
+var arrayItems = products.items;
+
+//this block for D1
+//var itemCount = 0;
+// for (var i = 0; i < arrayItems.length; i++) {
+//   if (arrayItems[i].kind === "shopping#product") {
+//     itemCount++;
+//     }
+//   };
+// console.log(itemCount);
+
+//this block for D1b
+// console.log("Count info retrieved from value of 'products.currentItemCount': " + products.currentItemCount);
+
+//this block for D2
+// for (var i = 0; i < arrayItems.length; i++) {
+//   if (arrayItems[i].product.inventories[0].availability === "backorder") {
+//       console.log(arrayItems[i].product.title)
+//     }
+//   };
+
+//this block for D3
+//for (var i = 0; i < arrayItems.length; i++) {
+//   if (arrayItems[i].product.images.length > 1) {
+//       console.log(arrayItems[i].product.title)
+//     }
+//   };
+
+//this is for D4
+//for (var i = 0; i < arrayItems.length; i++) {
+//   if (arrayItems[i].product.brand === "Canon") {
+//       console.log(arrayItems[i].product.title)
+//     }
+//   };
+
+//this is for D5
+// for (var i = 0; i < arrayItems.length; i++) {
+//   if ( (arrayItems[i].product.brand === "Canon") && (arrayItems[i].product.author.name.includes("eBay")) ) {
+//       console.log(arrayItems[i]);
+//     }
+//   };
+
+//this is for D6
+// for (var i = 0; i < arrayItems.length; i++) {
+//   console.log(arrayItems[i].product.brand + ", " + arrayItems[i].product.inventories[0].price + ", " + arrayItems[i].product.images[0].link);
+//   };
+
