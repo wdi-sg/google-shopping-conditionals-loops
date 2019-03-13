76b7024753e336b14a35c9403a337ec6bfa31b67
tartufu <ainurlindale@gmail.com>
2019-03-13 21:19:19 +0800

    added part 3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
-// Go through the items and find all results that have kind of shopping#product. Print the count of these results. Where else is this count information stored in the search results?
+// Q1 Go through the items and find all results that have kind of shopping#product. Print the count of these results. Where else is this count information stored in the search results?
 
+console.log(products) // View this in console developer to view data tree
 var shoppingProduct = 0;
 
 // products.items is an array of 25 indexes, which is why you need products.items[i]
@@ -10,7 +11,7 @@ for (i = 0; i < products.items.length; i++) {
   }
 }
 console.log(shoppingProduct);
-console.log(products)
+
 //Print the title of all items with a backorder availability in inventories.
 // inventories key is an array, must be back ordered
 
@@ -20,12 +21,26 @@ for (i = 0; i < products.items.length; i++) {
   }
 }
 
-console.log(products.items[1].product.inventories[0].availability)
+//Print the title of all items with more than one image link.
+
+for (i = 0; i < products.items.length; i++) {
+  if (products.items[i].product.images.length > 1 ) {
+    console.log(products.items[i].product.title);
+  }
+} // use .length as images is an array.
 
 
 
 
-console.log(products.items[1].kind)
-console.log(products.items[0].kind)
 
+
+
+//console.log(products.items[1].product.inventories[0].availability)
+
+
+
+
+//console.log(products.items[1].kind)
+//console.log(products.items[0].kind)
+//console.log(products.items[0].product.images.length);
 
