b49a2d1c8d690301ba1a83fbd3804c2f1dc11dbc
moses <moseslee90@gmail.com>
2019-03-13 02:07:33 -0700

    items with backorder availability

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ for (var i = 0; i < products["items"].length; i++) {
   }
 }
 console.log(count + " items with shopping#product tag in 'kind'");
+console.log("products with 'backorder' in 'availability' below");
+for (var i = 0; i < products["items"].length; i++) {
+  if( products["items"][i].product.inventories[0].availability === "backorder") {
+    console.log(products["items"][i].product.title);
+  }
+}
