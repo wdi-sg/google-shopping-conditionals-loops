4abc06dd89d809a17ceba06d357d3e277c6d7496
Claire Elizabeth Seah <claireseah@Claires-MacBook-Air.local>
2019-03-13 21:45:50 +0800

    completed further statements

--- script.js
+++ script.js
@@ -6,4 +6,29 @@ for (i = 0; i < products.items.length; i++) {
    shoppingProduct ++;
  }
 }
+
 console.log(shoppingProduct);
+
+for (i = 0; i < products.items.length; i++) {
+ if (products.items[i].product.inventories[0].availability === "backorder") {
+   console.log(products.items[i].product.title);
+ }
+}
+
+console.log(products.items[1].product.inventories[0].availability);
+
+for (i = 0; i < products.items.length; i++) {
+	if (products.items[i].product.images > 1) {
+		console.log(products.items[i].product.title);
+	}
+}
+
+console.log(products.items[1].product.title);
+
+for (i = 0; i < products.items.length; i++) {
+	if (products.items[i].product.brand === "Canon") {
+		console.log(products.items[i].product.title);
+	}
+}
+
+console.log(products.items[1].product.title);
