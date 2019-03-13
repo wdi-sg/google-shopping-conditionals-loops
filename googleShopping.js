5bacc8e284b4f126f31fd927f392427caf3ea5c2
ViAyDiNe <neel.augusthy@gmail.com>
2019-03-13 22:58:25 +0800

    Question 1 & 2

---
+++ googleShopping.js
@@ -0,0 +1,32 @@
+console.log("hello");
+
+console.log(products);
+
+//Question 1
+console.log("Question 1");
+var count = 0
+
+for (var i = 0; i < products["items"].length; i++) {
+  var product = products["items"][i];
+  if (product["kind"] === "shopping#product"){
+    count++;
+  }
+}
+  console.log("Count base on loop: " + count);
+  console.log("Count retrived from search result: " + products["currentItemCount"]);
+
+//Question 2
+console.log("Question 2");
+
+for (var i = 0; i < products["items"].length; i++) {
+  var item = products["items"][i];
+  var inventory = item.product
+  if (inventory.inventories[0].availability === "backorder") {
+  console.log(inventory.title);
+  }
+}
+
+//Question 3
+console.log("Question 3");
+
+
