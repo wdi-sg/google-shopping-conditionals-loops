9287126be432f2defb109c6fa2c3589f25cc8192
Muhdhatta <muhdhatta.alchemist@gmail.com>
2019-03-14 10:01:44 +0800

    done

---
+++ script.js
@@ -0,0 +1,42 @@
+// var plain = "abcdefghijklmnopqrstuvwxyz"
+// var cipher = "zyxwvutsrqponmlkjihgfedcba"
+
+
+
+  // var result = "";
+
+  // for (var i = plain.length; i >= 0; i--) {
+  //   result += plain.charAt(i);
+  // }
+
+  // console.log( result );
+
+
+
+  var atbash = [
+  {h:'s'},
+  {e:'v'},
+  {l:'o'},
+  {o:'l'}
+  ];
+
+  for( var i=0; i<atbash.length; i++){
+    var saying = atbash[i].h
+  console.log( saying );
+}
+  for( var i=0; i<atbash.length; i++){
+    var saying = atbash[i].e
+  console.log( saying );
+}
+  for( var i=0; i<atbash.length; i++){
+    var saying = atbash[i].l
+  console.log( saying );
+}
+  for( var i=0; i<atbash.length; i++){
+    var saying = atbash[i].l
+  console.log( saying );
+}
+  for( var i=0; i<atbash.length; i++){
+    var saying = atbash[i].o
+  console.log( saying );
+}
