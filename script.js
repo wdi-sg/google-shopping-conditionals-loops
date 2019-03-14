19f5622cc3539ecdd6392aed68d84ac026cacedd
khyreerusydi <nicoraventbh@gmail.com>
2019-03-14 09:00:57 +0800

    updated script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-//this variable is used for all deliverables and furthers
+//submission not scalable, assumes data structure is known
+
+// //this variable is used for all deliverables and furthers
 var arrayItems = products.items;
 
-//this block for D1
+// //this block for D1
 var itemCount = 0;
 for (var i = 0; i < arrayItems.length; i++) {
   if (arrayItems[i].kind === "shopping#product") {
@@ -10,43 +12,44 @@ for (var i = 0; i < arrayItems.length; i++) {
   };
 console.log(itemCount);
 
-//this block for D1b
+// //this block for D1b
 console.log("Count info retrieved from value of 'products.currentItemCount': " + products.currentItemCount);
 
-//this block for D2
+// //this block for D2
 for (var i = 0; i < arrayItems.length; i++) {
   if (arrayItems[i].product.inventories[0].availability === "backorder") {
       console.log(arrayItems[i].product.title)
     }
   };
 
-//this block for D3
+// //this block for D3
 for (var i = 0; i < arrayItems.length; i++) {
   if (arrayItems[i].product.images.length > 1) {
       console.log(arrayItems[i].product.title)
     }
   };
 
-//this is for D4
+// //this is for D4
 for (var i = 0; i < arrayItems.length; i++) {
   if (arrayItems[i].product.brand === "Canon") {
       console.log(arrayItems[i].product.title)
     }
   };
 
-//this is for D5
+// //this is for D5
 for (var i = 0; i < arrayItems.length; i++) {
   if ( (arrayItems[i].product.brand === "Canon") && (arrayItems[i].product.author.name.includes("eBay")) ) {
-      console.log(arrayItems[i]);
+      // console.log(arrayItems[i]);
+      console.log(arrayItems[i].product.author.name);
     }
   };
 
-//this is for D6
+// //this is for D6
 for (var i = 0; i < arrayItems.length; i++) {
   console.log(arrayItems[i].product.brand + ", " + arrayItems[i].product.inventories[0].price + ", " + arrayItems[i].product.images[0].link);
   };
 
-//F1a
+// //F1a
 var brandInput = prompt("Which product brand are you looking for? Type in one option: Nikon, Sony, Canon, Lytro, Panasonic, Samsung, Fuji, Olympus, Pentax")
 
 for (var i = 0; i < arrayItems.length; i++) {
@@ -55,10 +58,10 @@ for (var i = 0; i < arrayItems.length; i++) {
   }
 };
 
-//F1b
+// //F1b
 var conditionInput = prompt("Would you like to see only new or used items?");
 
-//F2+3
+// //F2+3
 var nil = true;
 var searchInput = prompt("Would you like to search by 'Brand' or 'Condition'?");
 
@@ -86,5 +89,5 @@ else {
 };
 if (nil === true) {
   console.log("Sorry, no results found.")
-}
+};
 
