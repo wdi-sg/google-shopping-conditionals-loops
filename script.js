b51d8e2044a6349c9f3590af61db37722ffd83e3
Muhdhatta <muhdhatta.alchemist@gmail.com>
2019-03-14 12:35:06 +0800

    done

--- script.js
+++ script.js
@@ -1,42 +1,67 @@
-// var plain = "abcdefghijklmnopqrstuvwxyz"
-// var cipher = "zyxwvutsrqponmlkjihgfedcba"
+console.log(products);
 
-
-
-  // var result = "";
-
-  // for (var i = plain.length; i >= 0; i--) {
-  //   result += plain.charAt(i);
-  // }
-
-  // console.log( result );
-
-
-
-  var atbash = [
-  {h:'s'},
-  {e:'v'},
-  {l:'o'},
-  {o:'l'}
-  ];
-
-  for( var i=0; i<atbash.length; i++){
-    var saying = atbash[i].h
-  console.log( saying );
+var kind = "shopping#products";
+  var count1 = 0;
+for ( var i = 0; i < products.items.length; i++ ){
+  if (products.items[i].kind = "shopping#products"){
+    count1++;
+  }
 }
-  for( var i=0; i<atbash.length; i++){
-    var saying = atbash[i].e
-  console.log( saying );
+  console.log("Number of items with kind = shopping#products = " + count1);
+
+console.log("-----backorder");
+
+  var count2 = 0;
+for ( var i = 0; i < products.items.length; i++ ){
+  if (products.items[i].product.inventories[0].availability === "backorder"){
+    count2++
+
+    console.log(products.items[i].product.title);
+  }
 }
-  for( var i=0; i<atbash.length; i++){
-    var saying = atbash[i].l
-  console.log( saying );
+
+console.log("-----#ofBackorder");
+
+console.log("Number of items with availability = backorder = " + count2);
+
+console.log("-----moreImageLink");
+
+for ( var i = 0; i < products.items.length; i++ ){
+  if (products.items[i].product.images.length > 1){
+
+    console.log(products.items[i].product.title);
+  }
 }
-  for( var i=0; i<atbash.length; i++){
-    var saying = atbash[i].l
-  console.log( saying );
+console.log("-----Canon");
+
+for ( var i = 0; i < products.items.length; i++ ){
+  if (products.items[i].product.brand === "Canon"){
+
+    console.log(products.items[i].product.author.name);
+  }
 }
-  for( var i=0; i<atbash.length; i++){
-    var saying = atbash[i].o
-  console.log( saying );
+
+console.log("-----eBay+Canon");
+
+for ( var i = 0; i < products.items.length; i++ ){
+    if (products.items[i].product.author.name === "eBay" && products.items[i].product.brand === "Canon"){
+
+console.log("-----Brand+price+imageLink");
+
+
+    console.log(products.items[i].product.title);
+  }
 }
+for ( var i = 0; i < products.items.length; i++ ){
+
+
+
+
+//   console.log(products.items[i].product.brand)
+//   console.log(products.items[i].product.inventories[0].price)
+//   console.log(products.items[i].product.images[0].link);
+
+
+  console.log("Brand = " + products.items[i].product.brand + " Price: " + products.items[i].product.inventories[0].price + " Image link: " + products.items[i].product.images[0].link);
+
+  }
